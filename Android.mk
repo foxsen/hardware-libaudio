bfaae3d6ce8e4e77818819e5da55cec828d200a0
Zhang Fuxin <zhangfx@lemote.com>
2012-06-23 22:26:43 +0800

    change module name from audio.primary.default to audio.primary.$(TARGET_PRODUCT)

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio.primary.default
+LOCAL_MODULE := audio.primary.$(TARGET_PRODUCT)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
